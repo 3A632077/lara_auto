91463acd6fa4c928d45f1a3984c3dc3b3e61a278
林威震 <weizhen19990727@gmail.com>
2019-10-20 15:02:49 +0800

    修改index.php程式碼內容

--- public/index.php
+++ public/index.php
@@ -10,5 +10,5 @@ $world = new World();
 $lara = new Lara();
 $vincent = new Hello\Someone('Vincent');
 // 以下Someone類別的使用可以不用use Demo\Hello;
-$mary = new \Demo\Hello\Someone('Mary');
+$mary = new Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
